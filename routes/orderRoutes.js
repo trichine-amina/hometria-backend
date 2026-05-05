817fde18f8d75039a9104fc88faf9d4fb3127c1d
amina <minatrichine19@gmail.com>
2026-05-05 20:42:32 +0100

    Allow guest orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,8 +3,8 @@ const router  = express.Router();
 const ctrl    = require('../controllers/orderController');
 const { protect } = require('../middleware/authMiddleware');
 
-// Criterion 4: all order routes are protected
-router.post('/',    protect, ctrl.createOrder);
+// POST is now public - no login required for guest checkout
+router.post('/',    ctrl.createOrder);
 router.get('/',     protect, ctrl.getAllOrders);
 router.get('/:id',  protect, ctrl.getOrderById);
 
